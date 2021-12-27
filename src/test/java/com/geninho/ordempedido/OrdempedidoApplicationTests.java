7ed4b63144c4e80b63506a0b9448d89145b63f22
geniinho@hotmail.com <18031993>
2021-12-27 12:36:27 -0300

    projeto criado

---
+++ src/test/java/com/geninho/ordempedido/OrdempedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geninho.ordempedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdempedidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
