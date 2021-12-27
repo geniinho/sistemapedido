b5ee6add36f11acbe48490d8af70a5e27a7a4e6f
geniinho@hotmail.com <18031993>
2021-12-27 12:36:46 -0300

    CategoriaResources criado e testado

---
+++ src/main/java/com/geninho/ordempedido/domain/Categoria.java
@@ -0,0 +1,20 @@
+package com.geninho.ordempedido.domain;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+
+}
