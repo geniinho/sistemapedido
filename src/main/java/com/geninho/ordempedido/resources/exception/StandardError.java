28aed03087cbe4c6e1ebbdd56958e965e9ae658c
geniinho@hotmail.com <18031993>
2021-12-30 13:49:38 -0300

    handler para tratamento de exceção

--- src/main/java/com/geninho/ordempedido/resources/exception/StandardError.java
+++ src/main/java/com/geninho/ordempedido/resources/exception/StandardError.java
@@ -8,8 +8,7 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Getter
 @Setter
 public class StandardError implements Serializable {
@@ -19,4 +18,11 @@ public class StandardError implements Serializable {
     private Integer status;
     private String msg;
     private Long timeStamp;
+
+    public StandardError(Integer status, String msg, Long timeStamp) {
+        super();
+        this.status = status;
+        this.msg = msg;
+        this.timeStamp = timeStamp;
+    }
 }
