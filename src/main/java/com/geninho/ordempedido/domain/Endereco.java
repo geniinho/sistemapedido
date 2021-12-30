1ff064884a80c725ecbf8711ce2f5b9fd9ca6bad
geniinho@hotmail.com <18031993>
2021-12-30 15:49:15 -0300

    classe de dominio Cliente e Endereco

---
+++ src/main/java/com/geninho/ordempedido/domain/Endereco.java
@@ -0,0 +1,42 @@
+package com.geninho.ordempedido.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+public class Endereco implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+
+    private Cliente cliente;
+
+    private Cidade cidade;
+
+    public Endereco() {
+    }
+
+    public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep, Cliente cliente, Cidade cidade) {
+        super();
+        this.id = id;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cliente = cliente;
+        this.cidade = cidade;
+    }
+}
