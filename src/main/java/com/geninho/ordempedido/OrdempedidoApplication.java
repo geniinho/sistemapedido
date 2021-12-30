ea217be9c262b58b0530ed8c9da4df8a7c1345d7
geniinho@hotmail.com <18031993>
2021-12-30 13:33:26 -0300

    dados mockados para teste

--- src/main/java/com/geninho/ordempedido/OrdempedidoApplication.java
+++ src/main/java/com/geninho/ordempedido/OrdempedidoApplication.java
@@ -26,22 +26,22 @@ public class OrdempedidoApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-//        Categoria cat1 = new Categoria(null, "Informática");
-//        Categoria cat2 = new Categoria(null, "Escritório");
-//
-//        Produto p1 = new Produto(null, "Computador", 2000.00);
-//        Produto p2 = new Produto(null, "Impressora", 800.00);
-//        Produto p3 = new Produto(null, "Mouse", 80.00);
-//
-//        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-//        cat2.getProdutos().add(p2);
-//
-//        p1.getCategorias().add(cat1);
-//        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-//        p3.getCategorias().add(cat1);
-//
-//
-//        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
-//        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+        cat2.getProdutos().add(p2);
+
+        p1.getCategorias().add(cat1);
+        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+        p3.getCategorias().add(cat1);
+
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
     }
 }
