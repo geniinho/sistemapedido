ebb789884380ec13d212d7391b458ce768880c6c
geniinho@hotmail.com <18031993>
2022-01-20 07:48:35 -0300

    ItemPedidoRepository

--- src/main/java/com/geninho/ordempedido/domain/Cliente.java
+++ src/main/java/com/geninho/ordempedido/domain/Cliente.java
@@ -1,53 +1,73 @@
 package com.geninho.ordempedido.domain;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.geninho.ordempedido.domain.enums.TipoCliente;
-import lombok.EqualsAndHashCode;
-
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@EqualsAndHashCode
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.geninho.ordempedido.domain.enums.TipoCliente;
+
 @Entity
 public class Cliente implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @Column(unique=true)
     private String email;
     private String cpfOuCnpj;
     private Integer tipo;
 
+    @JsonIgnore
+    private String senha;
 
-    @JsonManagedReference
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
-    @CollectionTable(name = "TELEFONE")
+    @CollectionTable(name="TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
-    @OneToMany(mappedBy = "cliente")
+    @ElementCollection(fetch=FetchType.EAGER)
+    @CollectionTable(name="PERFIS")
+    private Set<Integer> perfis = new HashSet<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy="cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
-    public Cliente() {
-    }
 
-    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
         super();
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.cpfOuCnpj = cpfOuCnpj;
-        this.tipo = tipo.getCod();
+        this.tipo = (tipo==null) ? null : tipo.getCod();
+        this.senha = senha;
+
+    }
+
+    public Cliente() {
+
     }
 
     public Integer getId() {
@@ -82,13 +102,23 @@ public class Cliente implements Serializable {
         this.cpfOuCnpj = cpfOuCnpj;
     }
 
-    public  TipoCliente getTipo() {
+    public TipoCliente getTipo() {
         return TipoCliente.toEnum(tipo);
     }
 
     public void setTipo(TipoCliente tipo) {
         this.tipo = tipo.getCod();
     }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
 
     public List<Endereco> getEnderecos() {
         return enderecos;
@@ -106,15 +136,36 @@ public class Cliente implements Serializable {
         this.telefones = telefones;
     }
 
-    public void setTipo(Integer tipo) {
-        this.tipo = tipo;
-    }
-
     public List<Pedido> getPedidos() {
         return pedidos;
     }
 
     public void setPedidos(List<Pedido> pedidos) {
         this.pedidos = pedidos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
