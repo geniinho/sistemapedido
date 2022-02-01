31e2766594c92661f15932ba3a00d6d3512cfea9
geniinho@hotmail.com <18031993>
2022-02-01 11:56:43 -0300

    arrumando CategoriaService e CategoriaResources

--- src/main/java/com/geninho/ordempedido/resources/CategoriaResources.java
+++ src/main/java/com/geninho/ordempedido/resources/CategoriaResources.java
@@ -1,83 +1,76 @@
 package com.geninho.ordempedido.resources;
 
-import com.geninho.ordempedido.domain.Categoria;
-import com.geninho.ordempedido.dto.CategoriaDTO;
-import com.geninho.ordempedido.services.CategoriaService;
-import com.geninho.ordempedido.services.Exception.DataIntegrityViolation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.geninho.ordempedido.domain.Categoria;
+import com.geninho.ordempedido.dto.CategoriaDTO;
+import com.geninho.ordempedido.services.CategoriaService;
 
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping(value="/categorias")
 public class CategoriaResources {
 
     @Autowired
     private CategoriaService service;
 
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+        Categoria obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
     @PostMapping
-    public ResponseEntity<Void> insert (@Valid @RequestBody CategoriaDTO objDto){
+    public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
         Categoria obj = service.fromDTO(objDto);
         obj = service.insert(obj);
-        //retornando uri do obj criando para o cliente
-        URI uri = ServletUriComponentsBuilder.
-                fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @GetMapping
-    public ResponseEntity<List<CategoriaDTO>> findAll(){
-        List<Categoria> list = service.findAll();
-        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO()).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDto);
 
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<?> find(@PathVariable Integer id){
-        Categoria obj = service.find(id);
-        return ResponseEntity.ok().body(obj);
-
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto,@PathVariable Integer id){
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
         Categoria obj = service.fromDTO(objDto);
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id){
-        try{
-            service.delete(id);
-        }
-        catch (DataIntegrityViolationException ex){
-            throw new DataIntegrityViolation("Não é possivel excluir categoria que já possui produtos cadastrados");
-        }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping(value = "/page")
-    public ResponseEntity<Page> findPage(
-            @RequestParam(value = "page",defaultValue = "0") Integer page,
-            @RequestParam(value = "linePerPage",defaultValue = "24") Integer linesPerPage,
-            @RequestParam(value = "orderBy",defaultValue = "nome") String orderBy,
-            @RequestParam(value = "direction",defaultValue = "ASC") String direction){
-
-        Page<Categoria> list = service.findPage(page,linesPerPage,orderBy,direction);
-        Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO());
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
 
+    @GetMapping(value="/page")
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value="page", defaultValue="0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+            @RequestParam(value="direction", defaultValue="ASC") String direction) {
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+        return ResponseEntity.ok().body(listDto);
+    }
 }
--- src/main/java/com/geninho/ordempedido/services/CategoriaService.java
+++ src/main/java/com/geninho/ordempedido/services/CategoriaService.java
@@ -1,17 +1,21 @@
 package com.geninho.ordempedido.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
 import com.geninho.ordempedido.domain.Categoria;
 import com.geninho.ordempedido.dto.CategoriaDTO;
 import com.geninho.ordempedido.repositories.CategoriaRepository;
+import com.geninho.ordempedido.services.Exception.DataIntegrityViolation;
 import com.geninho.ordempedido.services.Exception.ObjectNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -19,42 +23,47 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria find (Integer id){
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElseThrow(
-                () -> new ObjectNotFoundException(
-                        "Objeto não encontrado."
-                )
-        );
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado!"));
     }
 
-    public Categoria insert (Categoria obj){
+    public Categoria insert(Categoria obj) {
         obj.setId(null);
         return repo.save(obj);
     }
 
-    public Categoria update(Categoria obj){
-        find(obj.getId());
-        return repo.save(obj);
+    public Categoria update(Categoria obj) {
+        Categoria newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
     }
 
-    public void delete(Integer id){
+    public void delete(Integer id) {
         find(id);
-        repo.deleteById(id);
+        try {
+            repo.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolation("Não é possível excluir uma categoria que possui produtos");
+        }
     }
 
-    public List<Categoria> findAll(){
+    public List<Categoria> findAll() {
         return repo.findAll();
     }
 
-
-    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
         return repo.findAll(pageRequest);
     }
 
-    public Categoria fromDTO(CategoriaDTO objDto){
+    public Categoria fromDTO(CategoriaDTO objDto) {
         return new Categoria(objDto.getId(), objDto.getNome());
     }
 
+    private void updateData(Categoria newObj, Categoria obj) {
+        newObj.setNome(obj.getNome());
+    }
 }
