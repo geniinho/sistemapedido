8865e690311ccc8cc707972c731fa557c86a9ce3
geniinho@hotmail.com <18031993>
2022-06-04 20:09:58 -0300

    inserindo pedido

--- src/main/java/com/geninho/ordempedido/resources/PedidoResources.java
+++ src/main/java/com/geninho/ordempedido/resources/PedidoResources.java
@@ -5,6 +5,10 @@ import com.geninho.ordempedido.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -19,5 +23,13 @@ public class PedidoResources {
         Pedido obj = service.find(id);
         return ResponseEntity.ok().body(obj);
 
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/geninho/ordempedido/services/PedidoService.java
+++ src/main/java/com/geninho/ordempedido/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.geninho.ordempedido.services;
 
+import com.geninho.ordempedido.domain.ItemPedido;
+import com.geninho.ordempedido.domain.PagamentoComBoleto;
 import com.geninho.ordempedido.domain.Pedido;
+import com.geninho.ordempedido.domain.enums.EstadoPagamento;
+import com.geninho.ordempedido.repositories.ItemPedidoRepository;
+import com.geninho.ordempedido.repositories.PagamentoRepository;
 import com.geninho.ordempedido.repositories.PedidoRepository;
+import com.geninho.ordempedido.repositories.ProdutoRepository;
 import com.geninho.ordempedido.services.Exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -14,6 +21,18 @@ public class PedidoService {
     @Autowired
     private PedidoRepository repo;
 
+    @Autowired
+    private BoletoService boletoService;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
     public Pedido find (Integer id){
         Optional<Pedido> obj = repo.findById(id);
         return obj.orElseThrow(
@@ -21,5 +40,26 @@ public class PedidoService {
                         "Objeto não encontrado."
                 )
         );
+    }
+
+    public Pedido insert(Pedido obj){
+        obj.setId(null);
+        obj.setInstante(new Date());
+        obj.getPagamento().setEstado(EstadoPagamento.PENDENTTE);
+        obj.getPagamento().setPedido(obj);
+        if (obj.getPagamento() instanceof PagamentoComBoleto){
+            PagamentoComBoleto pagamentoComBoleto = (PagamentoComBoleto) obj.getPagamento();
+            boletoService.preencherPagamentoComBoleto(pagamentoComBoleto, obj.getInstante());
+        }
+        obj = repo.save(obj);
+        pagamentoRepository.save(obj.getPagamento());
+        for (ItemPedido ip : obj.getItens()){
+            ip.setDesconto(0.0);
+            ip.setProduto(produtoService.find(ip.getProduto().getId()));
+            ip.setPreco(ip.getProduto().getPreco());
+            ip.setPedido(obj);
+        }
+        itemPedidoRepository.saveAll(obj.getItens());
+        return obj;
     }
 }
