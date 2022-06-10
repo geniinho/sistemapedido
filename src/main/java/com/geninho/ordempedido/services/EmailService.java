6462a71e88be5a2f24f3e142530636b2c4a83e67
Ingrid Santos <ingrid5889@gmail.com>
2022-06-10 12:11:24 -0300

    envio de email com html

--- src/main/java/com/geninho/ordempedido/services/EmailService.java
+++ src/main/java/com/geninho/ordempedido/services/EmailService.java
@@ -3,7 +3,11 @@ package com.geninho.ordempedido.services;
 import com.geninho.ordempedido.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
