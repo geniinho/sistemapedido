6877308897ddbae5ba81bea2ea0305ad2a11c2fe
Genivaldo Alves <geniinho@hotmail.com>
2022-06-13 17:02:23 -0300

    Esqueci a senha

--- src/main/java/com/geninho/ordempedido/resources/AuthResource.java
+++ src/main/java/com/geninho/ordempedido/resources/AuthResource.java
@@ -1,16 +1,20 @@
 package com.geninho.ordempedido.resources;
 
+import com.geninho.ordempedido.dto.EmailDTO;
 import com.geninho.ordempedido.security.JWTUtil;
 import com.geninho.ordempedido.security.UserSS;
+import com.geninho.ordempedido.services.AuthService;
 import com.geninho.ordempedido.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -19,11 +23,20 @@ public class AuthResource {
     @Autowired
     JWTUtil jwtUtil;
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/refresh_token")
     public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
         UserSS user = UserService.authenticated();
         String token = jwtUtil.generateToken(user.getUsername());
         response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/forgot")
+    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+        authService.sendNewPassword(objDTO.getEmail());
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/geninho/ordempedido/services/EmailService.java
+++ src/main/java/com/geninho/ordempedido/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.geninho.ordempedido.services;
 
+import com.geninho.ordempedido.domain.Cliente;
 import com.geninho.ordempedido.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -10,4 +11,5 @@ public interface EmailService {
     void sendEmail(SimpleMailMessage msg);
     void sendOrderConfirmationHtmlEmail(Pedido obj);
     void sendHtmlEmail(MimeMessage msg);
+    void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
