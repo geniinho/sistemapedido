7fece56f63f2cfeccf17c7eca585de1c7fccbcfd
Genivaldo Alves <geniinho@hotmail.com>
2022-06-14 16:00:51 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/geninho/ordempedido/services/ClienteService.java
+++ src/main/java/com/geninho/ordempedido/services/ClienteService.java
@@ -50,6 +50,9 @@ public class ClienteService {
     @Value("${img.prefix.client.profile}")
     private String prefix;
 
+    @Value("${img.profile.size}")
+    private Integer size;
+
     public Cliente find(Integer id) {
         UserSS user = UserService.authenticated();
         if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())){
@@ -136,6 +139,8 @@ public class ClienteService {
         }
         BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
         String fileName = prefix + user.getId() + ".jpg";
+        jpgImage = imageService.cropSquare(jpgImage);
+        jpgImage = imageService.resize(jpgImage, size);
         return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
     }
 }
